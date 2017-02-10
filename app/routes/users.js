cfd0678708875db5f23c2ccf6a81da09dc0bf7d7
Juan Alexis Guzman <alexis.guzman27@gmail.com>
2017-02-10 17:10:28 -0600

    api calls works now, but text has to be exactly the same

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var _ = require('lodash');
 var fs = require('fs'),json;
 
 router.get('/', function(req, res){
@@ -10,20 +11,60 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
     res.json({ message: 'Contains data from users registered in the system.' });
 });
+router.get('/test', function(req, res){
+    var myjson = 'https://api.myjson.com/bins/usey5';
+    request(myjson, function(err, body){
+      res.json({message:body}); //res is the response object, and it passes info back to client side
+  });
+});
 
 router.get('/:id', function(req, res){
-    //var pathToJson = './app/data/test.json'
-    var pathToJson = './app/data/dummy.json'
+    var pathToJson = './app/data/test.json';
     fs.readFile(pathToJson, (err, data) => {
         if (err) throw err;
         var obj = JSON.parse(data);
         var idUser = req.params.id;
-        //console.log(obj);
-        console.log(obj.members);
-        //console.log(obj.members.indexOf(idUser));        
+        var userModel = _.find(obj.users, function(o) { return o.id == idUser; });
+        console.log(userModel);
         res.json({
-            message: 'Hola ' + idUser,
-            data: obj
+            message: 'Su id es ' + idUser,
+            data: userModel
+        });
+    });
+});
+router.get('/name/:name', function(req, res){
+    var pathToJson = './app/data/test.json';
+    fs.readFile(pathToJson, (err, data) => {
+        if (err) throw err;
+        var obj = JSON.parse(data);
+        var paramUrl = req.params.name;
+        console.log(obj.users);
+        var userModel = _.find(obj.users, function(o) { 
+            console.log(paramUrl);
+            return o.firstName.toLowerCase() == paramUrl.toLowerCase()
+        });
+        console.log(userModel);
+        res.json({
+            message: 'Hola ' + paramUrl,
+            data: userModel
+        });
+    });
+});
+router.get('/lastName/:lastname', function(req, res){
+    var pathToJson = './app/data/test.json';
+    fs.readFile(pathToJson, (err, data) => {
+        if (err) throw err;
+        var obj = JSON.parse(data);
+        var paramUrl = req.params.lastname;
+        console.log(obj.users);
+        var userModel = _.filter(obj.users, function(o) { 
+            console.log(paramUrl.toString());
+            return o.lastName.toLowerCase() == paramUrl.toLowerCase()
+        });
+        console.log(userModel);
+        res.json({
+            message: 'Hola ' + paramUrl,
+            data: userModel
         });
     });
 });
