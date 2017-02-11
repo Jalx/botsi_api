cac590728be0820462dcd3019ceef08f4a0d3c47
Juan Alexis Guzman <alexis.guzman27@gmail.com>
2017-02-11 01:20:33 -0600

    added request module, and added request to json file on github

--- app/routes/paquetes.js
+++ app/routes/paquetes.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var request = require('request');
 var _ = require('lodash');
 var fs = require('fs'),json;
 
 pathToJson = './app/data/paquetes.json';
+urlToJson = "https://raw.githubusercontent.com/Jalx/botsi_api/master/app/data/paquetes.json";
 
 router.get('/', function(req, res){res.json({ message: 'Products from package model' });});
 
@@ -45,44 +47,48 @@ router.get('/:id', function(req, res){
 });
 
 router.get('/query/:paramNm/:paramKw', function(req, res){
-    fs.readFile(pathToJson, (err, data) => {
-        if (err) throw err;
-        var obj = JSON.parse(data);
-        var queryParamKw = req.params.paramKw;
-        var queryParamNm = req.params.paramNm;
-        console.log(queryParamKw + ' ' + queryParamNm);
-        var collection = _.filter(obj.packages, function(o) { return _.includes(o.client, queryParamNm);});
-        var indexModel = _.findIndex(collection, function(o) { return _.includes(o.keywords, queryParamKw); });
-        var model = collection[indexModel];
-        //var model = _.includes(obj.packages, queryParamKw);
-        console.log(collection);
-        console.log(model);
-        if (model.statusESA == false){
-            var placeholderData = 'Su paquete aun no se encuentra en el país.'            
+    request({
+        url: urlToJson,
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var queryParamKw = req.params.paramKw;
+            var queryParamNm = req.params.paramNm;
+            console.log(queryParamKw + ' ' + queryParamNm);
+            var collection = _.filter(body.packages, function(o) { return _.includes(o.client, queryParamNm);});
+            var indexModel = _.findIndex(collection, function(o) { return _.includes(o.keywords, queryParamKw); });
+            var model = collection[indexModel];
+            console.log(collection);
+            console.log(model);
+            if (model.statusESA == false){
+                var placeholderData = 'Su paquete aun no se encuentra en el país.'            
+            } else {
+                var placeholderData = 'Su paquete ya esta disponible para ser retirado :)'                        
+            }
+            res.json({
+                "messages": [
+                    {
+                        "text": placeholderData,
+                        "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "button",
+                                "text": "Hello!",
+                                "buttons": [
+                                    {
+                                    "type": "web_url",
+                                    "url": model.urlItem,
+                                    "title": "Ver Item"
+                                    }
+                                ]
+                            }
+                        },
+                    }
+                ]
+            });
         } else {
-            var placeholderData = 'Su paquete ya esta disponible para ser retirado :)'                        
+            console.log('shit D:');
         }
-        res.json({
-            "messages": [
-                {
-                    "text": placeholderData,
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "button",
-                            "text": "Hello!",
-                            "buttons": [
-                                {
-                                "type": "web_url",
-                                "url": model.urlItem,
-                                "title": "Ver Item"
-                                }
-                            ]
-                        }
-                    },
-                }
-            ]
-        });
     });
 });
 
