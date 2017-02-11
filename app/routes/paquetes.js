6ca3b995874960fff92bcb67781c39ebe09e2f2f
Juan Alexis Guzman <alexis.guzman27@gmail.com>
2017-02-11 01:03:14 -0600

    added some kind of search functionality

--- app/routes/paquetes.js
+++ app/routes/paquetes.js
@@ -22,22 +22,65 @@ router.get('/:id', function(req, res){
         console.log(model);
         res.json({
             "messages": [
-                {"text": placeholderData}
+                {
+                    "text": placeholderData,
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "button",
+                            "text": "Hello!",
+                            "buttons": [
+                                {
+                                "type": "web_url",
+                                "url": model.urlItem,
+                                "title": "Ver Item"
+                                }
+                            ]
+                        }
+                    },
+                }
             ]
         });
     });
 });
 
-router.get('/:id', function(req, res){
+router.get('/query/:paramNm/:paramKw', function(req, res){
     fs.readFile(pathToJson, (err, data) => {
         if (err) throw err;
         var obj = JSON.parse(data);
-        var idPack = req.params.id;
-        var model = _.find(obj.packages, function(o) { return o.id == idPack; });
+        var queryParamKw = req.params.paramKw;
+        var queryParamNm = req.params.paramNm;
+        console.log(queryParamKw + ' ' + queryParamNm);
+        var collection = _.filter(obj.packages, function(o) { return _.includes(o.client, queryParamNm);});
+        var indexModel = _.findIndex(collection, function(o) { return _.includes(o.keywords, queryParamKw); });
+        var model = collection[indexModel];
+        //var model = _.includes(obj.packages, queryParamKw);
+        console.log(collection);
+        console.log(model);
+        if (model.statusESA == false){
+            var placeholderData = 'Su paquete aun no se encuentra en el país.'            
+        } else {
+            var placeholderData = 'Su paquete ya esta disponible para ser retirado :)'                        
+        }
         res.json({
             "messages": [
-                {"text": "Su id es " + idPack},
-                {"text": model}
+                {
+                    "text": placeholderData,
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "button",
+                            "text": "Hello!",
+                            "buttons": [
+                                {
+                                "type": "web_url",
+                                "url": model.urlItem,
+                                "title": "Ver Item"
+                                }
+                            ]
+                        }
+                    },
+                }
             ]
         });
     });
